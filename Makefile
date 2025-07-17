dea984e8e3f5fe77c38e17bdf3dd71e57df32e62
Ruxandra Simoiu <maria.simoiu@stud..acs.upb.ro>
2025-07-17 11:00:30 +0300

    interfata

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 TARGET  =  biblioteca
 
 COMPILER = gcc
-CFLAGS = -Wall -g -Wextra
+CFLAGS = -Wall -g -Wextra `pkg-config --cflags gtk+-3.0`
+LDFLGS = `pkg-config --libs gtk+-3.0`
 
 SRC = main.c functii.c 
 
 build:
-	$(COMPILER) $(CFLAGS) $(SRC) -o $(TARGET)
+	$(COMPILER) $(CFLAGS) $(SRC) -o $(TARGET) $(LDFLGS)
 
 run: build
 	./$(TARGET)
--- main.c
+++ main.c
@@ -1,84 +1,180 @@
+#include <gtk/gtk.h>
 #include "functii.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int main(){
-    biblioteca bib = aloc_bibl();
-    // // int nr_carti = 100;
-    int nr_genuri = 6;
-    int nr_cititori = 5;
-    char* nume = malloc(50);
-    // // nume = "Biblioteca lui Ruxi";
-    strcpy(nume, "Biblioteca lui Ruxi");
-    init_bibl(bib, nr_genuri, nume, nr_cititori);
-    afisez_biblioteca(bib);
+biblioteca bibl;
 
-    printf("\n");
+void afiseaza_carti(gpointer data) {
+    GtkTextBuffer *buffer = GTK_TEXT_BUFFER(data);
+    gtk_text_buffer_set_text(buffer, "", -1);
+    GtkTextIter iter;
 
-    carte book = aloc_carte();
+    for (int i = 0; i < bibl->nr_carti; i++) {
+        gchar *linie = g_strdup_printf("Titlu: %s | Autor: %s | Pagini: %d\n",
+            bibl->toate_cartile[i]->titlu,
+            bibl->toate_cartile[i]->autor,
+            bibl->toate_cartile[i]->nr_pagini);
+        gtk_text_buffer_get_end_iter(buffer, &iter);
+        gtk_text_buffer_insert(buffer, &iter, linie, -1);
+        g_free(linie);
+    }
+}
 
-    char* titlu = malloc(50);
-    strcpy(titlu, "One Day");
-    char* autor = malloc(50);
-    strcpy(autor, "David Nicholls");
-    char* sectiune = malloc(50);
-    strcpy(sectiune, "Fiction");
-    char* editura = malloc(50);
-    strcpy(editura, "Pinguin");
-    int nr_pagini = 448;
-    int index = 23;
+void afiseaza_cititori(gpointer data) {
+    GtkTextBuffer *buffer = GTK_TEXT_BUFFER(data);
+    gtk_text_buffer_set_text(buffer, "", -1);
+    GtkTextIter iter;
 
-    init_carte(book, titlu, autor, sectiune, editura, nr_pagini, index);
+    for (int i = 0; i < bibl->nr_cititori; i++) {
+        gchar *linie = g_strdup_printf("Nume: %s | Varsta: %d | Numar permis biblioteca: %d | Numar carti imprumutate: %d\n",
+            bibl->cititori[i]->nume,
+            bibl->cititori[i]->varsta,
+            bibl->cititori[i]->nr_permis_biblioteca,
+            bibl->cititori[i]->nr_carti_imprumutate);
+        gtk_text_buffer_get_end_iter(buffer, &iter);
+        gtk_text_buffer_insert(buffer, &iter, linie, -1);
+        g_free(linie);
+    }
+}
 
-    afisez_carte(book);
+void fereastra_adauga_carte(gpointer data) {
+    GtkWidget *dialog = gtk_dialog_new_with_buttons("Adaugă carte",
+        GTK_WINDOW(data),
+        GTK_DIALOG_MODAL,
+        "_Adaugă", GTK_RESPONSE_ACCEPT,
+        "_Renunță", GTK_RESPONSE_CANCEL,
+        NULL);
 
-    printf("\n");
+    GtkWidget *content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+    GtkWidget *grid = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(grid), 5);
+    gtk_grid_set_column_spacing(GTK_GRID(grid), 5);
+    gtk_container_add(GTK_CONTAINER(content_area), grid);
 
-    cititor reader = aloc_cititor();
+    GtkWidget *entry_titlu = gtk_entry_new();
+    GtkWidget *entry_autor = gtk_entry_new();
+    GtkWidget *entry_editura = gtk_entry_new();
+    GtkWidget *entry_pagini = gtk_entry_new();
+    GtkWidget *entry_sectiune = gtk_entry_new();
 
-    char *nume_cititor = malloc(10);
-    strcpy(nume_cititor, "Ruxi");
-    int varsta = 19;
-    int permis = 124;
-    // int nr_carti_imp = 4;
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Titlu:"), 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), entry_titlu, 1, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Autor:"), 0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), entry_autor, 1, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Editura:"), 0, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), entry_editura, 1, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Nr. pagini:"), 0, 3, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), entry_pagini, 1, 3, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Secțiune:"), 0, 4, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), entry_sectiune, 1, 4, 1, 1);
 
-    init_cititor(reader, nume_cititor, varsta, permis);
+    gtk_widget_show_all(dialog);
+    gint result = gtk_dialog_run(GTK_DIALOG(dialog));
 
-    afisez_cititor(reader);
+    if (result == GTK_RESPONSE_ACCEPT) {
+        const char *titlu = gtk_entry_get_text(GTK_ENTRY(entry_titlu));
+        const char *autor = gtk_entry_get_text(GTK_ENTRY(entry_autor));
+        const char *editura = gtk_entry_get_text(GTK_ENTRY(entry_editura));
+        const char *sectiune = gtk_entry_get_text(GTK_ENTRY(entry_sectiune));
+        int pagini = atoi(gtk_entry_get_text(GTK_ENTRY(entry_pagini)));
 
-    printf("----------------------------------------\n\n");
+        carte book = aloc_carte();
+        init_carte(book, strdup(titlu), strdup(autor), strdup(sectiune), strdup(editura), pagini, bibl->nr_carti);
+        adaug_sectiune(bibl, book->sectiune);  // adaugă dacă nu există
+        adaug_carte(book, bibl, book->sectiune);
+    }
 
-    adaug_carte(book, bib, sectiune);
-    adaug_sectiune(bib, sectiune);
-    adaug_carte(book, bib, sectiune);
-    afisez_sectune(bib, sectiune);
+    gtk_widget_destroy(dialog);
+}
 
-    adaug_cititor(bib, reader);
-    afisez_cititor(bib->cititori[bib->nr_cititori - 1]);
-    // afisez_toate_cartile(bib);
+void fereastra_adauga_cititor(gpointer data) {
+    GtkWidget *dialog = gtk_dialog_new_with_buttons("Adaugă cititor",
+        GTK_WINDOW(data),
+        GTK_DIALOG_MODAL,
+        "_Adaugă", GTK_RESPONSE_ACCEPT,
+        "_Renunță", GTK_RESPONSE_CANCEL,
+        NULL);
 
-    printf("----------------------------------------\n\n");
-    // imprumut_carte(book, reader, bib);
-    // afisez_imprumut(reader);
-    // printf("----------------------------------------\n\n");
-    // return_carte(book, reader, bib);
-    // afisez_imprumut(reader);
+    GtkWidget *content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+    GtkWidget *grid = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(grid), 5);
+    gtk_grid_set_column_spacing(GTK_GRID(grid), 5);
+    gtk_container_add(GTK_CONTAINER(content_area), grid);
 
-    sterg_carte(book, bib);
-    sterg_cititor(reader, bib);
+    GtkWidget *entry_nume = gtk_entry_new();
+    GtkWidget *entry_varsta = gtk_entry_new();
+    GtkWidget *entry_permis = gtk_entry_new();
+    // GtkWidget *entry_pagini = gtk_entry_new();
+    // GtkWidget *entry_sectiune = gtk_entry_new();
 
-    
-    
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Nume:"), 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), entry_nume, 1, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Varsta:"), 0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), entry_varsta, 1, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Numar permis:"), 0, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), entry_permis, 1, 2, 1, 1);
 
-    // free_carte(book);
-    free(titlu);
-    free(autor);
-    free(sectiune);
-    free(editura);
-    // free_cititor(reader);
-    free(nume);
-    free_bibl(bib);
-    free(nume_cititor);
+    gtk_widget_show_all(dialog);
+    gint result = gtk_dialog_run(GTK_DIALOG(dialog));
+
+    if (result == GTK_RESPONSE_ACCEPT) {
+        const char *nume = gtk_entry_get_text(GTK_ENTRY(entry_nume));
+        const char *varsta = gtk_entry_get_text(GTK_ENTRY(entry_varsta));
+        const char *permis = gtk_entry_get_text(GTK_ENTRY(entry_permis));
+
+        cititor reader = aloc_cititor();
+        int varsta_int = atoi(varsta);
+        int permis_int = atoi(permis);
+        init_cititor(reader, strdup(nume), varsta_int, permis_int);
+        // adaug_sectiune(bibl, book->sectiune);  // adaugă dacă nu există
+        adaug_cititor(bibl, reader);
+    }
+
+    gtk_widget_destroy(dialog);
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    bibl = aloc_bibl();
+    init_bibl(bibl, 0, "Biblioteca GUI", 10);
+
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Biblioteca lui Ruxi");
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    GtkWidget *btn_adauga_carte = gtk_button_new_with_label("Adaugă carte");
+    gtk_box_pack_start(GTK_BOX(vbox), btn_adauga_carte, FALSE, FALSE, 0);
+
+    GtkWidget *btn_afiseaza = gtk_button_new_with_label("Afișează cărțile");
+    gtk_box_pack_start(GTK_BOX(vbox), btn_afiseaza, FALSE, FALSE, 0);
+
+    GtkWidget *btn_adauga_cititor = gtk_button_new_with_label("Adaugă cititor");
+    gtk_box_pack_start(GTK_BOX(vbox), btn_adauga_cititor, FALSE, FALSE, 0);
+
+    GtkWidget *btn_afiseaza_cititor = gtk_button_new_with_label("Afișează cititorii");
+    gtk_box_pack_start(GTK_BOX(vbox), btn_afiseaza_cititor, FALSE, FALSE, 0);
+
+    GtkWidget *scroll = gtk_scrolled_window_new(NULL, NULL);
+    gtk_widget_set_vexpand(scroll, TRUE);
+    gtk_box_pack_start(GTK_BOX(vbox), scroll, TRUE, TRUE, 0);
+
+    GtkWidget *text_view = gtk_text_view_new();
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE);
+    gtk_container_add(GTK_CONTAINER(scroll), text_view);
+
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+    g_signal_connect(btn_afiseaza, "clicked", G_CALLBACK(afiseaza_carti), buffer);
+    g_signal_connect(btn_adauga_carte, "clicked", G_CALLBACK(fereastra_adauga_carte), window);
+    g_signal_connect(btn_afiseaza_cititor, "clicked", G_CALLBACK(afiseaza_cititori), buffer);
+    g_signal_connect(btn_adauga_cititor, "clicked", G_CALLBACK(fereastra_adauga_cititor), window);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    free_bibl(bibl);
     return 0;
 }
